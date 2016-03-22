add781469f6d5dbd77b2d9e1b75307ac7c925d09
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-03-22 22:24:47 +0100

    [HackerRank/Algorithms] solve Warmup/Solve Me First problem

---
+++ HackerRank/Algorithms/Warmup/solve_me_first.py
@@ -0,0 +1,9 @@
+def solve_me_first(a, b):
+    """ Scan 2 integers from STDIN and return their sum on STDOUT """
+    return a + b
+
+num1 = input()
+num2 = input()
+res = solve_me_first(num1, num2)
+print res
+
